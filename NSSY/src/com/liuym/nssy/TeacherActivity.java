1bcc3d44d42d7640edf1db9a9178fcda93ca6390
liuym <liuyaming1003@gmail.com>
2014-08-22 17:56:53 +0800

    add images

--- NSSY/src/com/liuym/nssy/TeacherActivity.java
+++ NSSY/src/com/liuym/nssy/TeacherActivity.java
@@ -4,17 +4,25 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;;
 
 public class TeacherActivity extends Activity {
 	private Navigation navi = null;
+	private View teackerView = null;
+	private View ibeactonView = null;
+	private boolean ibViewIsFirst = true;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_teacher);
+		//setContentView(R.layout.activity_teacher);
 
+		LayoutInflater inflater = LayoutInflater.from(this); 
+		teackerView = inflater.inflate(R.layout.activity_teacher, null);
+		ibeactonView = inflater.inflate(R.layout.activity_ibeacon, null);
+		
 		navi = (Navigation)findViewById(R.id.navigationView);
 		navi.getBtn_left().setOnClickListener(new OnClickListener() {			
 			@Override
@@ -30,6 +38,8 @@ public class TeacherActivity extends Activity {
 			}
 		});
 		
+		findViewById(R.id.first).setEnabled(true);
+		
 		findViewById(R.id.teacherBtn).setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
@@ -40,11 +50,34 @@ public class TeacherActivity extends Activity {
 		findViewById(R.id.second).setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
-				changedActivity(Ibeacon.class);
+				setIbeaconView();
 			}
 		});
-		
-		
+	}
+	
+	private void setTeackerView(){
+		setContentView(teackerView);
+	}
+	
+	private void setIbeaconView(){
+		if(ibViewIsFirst){
+			ibViewIsFirst = false;
+			findViewById(R.id.logout).setOnClickListener(new OnClickListener() {			
+				@Override
+				public void onClick(View v) {
+					popRoot();
+				}
+			});
+			
+			findViewById(R.id.second).setEnabled(true);
+			
+			findViewById(R.id.first).setOnClickListener(new OnClickListener() {			
+				@Override
+				public void onClick(View v) {
+					setTeackerView();
+				}
+			});
+		}
 	}
 	
 	private void changedActivity(Class<?> inClass){
