db84ca60c71ec5d29d0cedd8e36fbb53a67cb9f2
Kermit Mei <kermit.mei@gmail.com>
2014-10-15 18:30:04 +0800

    add new for liuym at 1014

---
+++ NssyNIassisent/src/com/liuym/worker/OrderHandleActivity.java
@@ -0,0 +1,12 @@
+package com.liuym.worker;
+
+import android.os.Bundle;
+
+import com.liuym.nssyniassisent.MainActivity;
+
+class OrderHandleActivity extends MainActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
